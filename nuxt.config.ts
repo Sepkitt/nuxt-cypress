468d83b5f960039119dc562437d3854709b48bfa
Jermaine Sepkitt <jermaine.sepkitt@altron.com>
2023-09-27 14:05:14 +0200

    added dynamic component loader and vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+    // css: ['vuetify/lib/styles/main.sass', '@/assets/scss/main.scss', '@mdi/font/css/materialdesignicons.min.css'],
+    build: {
+      transpile: ["vuetify"],
+    },
+  vite: { ssr: { noExternal: ["vuetify"] } },
+
+});
---
+++ plugins/vuetify.ts
@@ -0,0 +1,54 @@
+// plugins/vuetify.js
+import { createVuetify, ThemeDefinition } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { mdi } from "vuetify/iconsets/mdi";
+
+import "@fontsource/roboto-mono";
+import "helvatica-neue-lt/index.css";
+import "@mdi/font/css/materialdesignicons.css";
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    text: "#707070",
+    primary: "#6c63ff",
+    accent: "#C0F1FF",
+    posbg: "#2B395D",
+    tertiary: "#282A2D",
+    background: "#151B27",
+    info: "#161B26",
+    screen: "#1B212E",
+    secondary: "#fb8c00",
+    warning: "#FE9E74",
+    error: "#fb8c00",
+    success: "#34BE82",
+    keypadBtn: "#212837",
+    bright: "#E2E2E2",
+    services: "#2F3A53",
+    totals: '#8b909e',
+    change: '#B71C1C' 
+  }
+};
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+
+    theme: {
+      defaultTheme: "darkTheme",
+      themes: {
+        darkTheme,
+      },
+    },
+    icons: {
+      defaultSet: "mdi",
+      sets: {
+        mdi,
+      },
+    },
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
